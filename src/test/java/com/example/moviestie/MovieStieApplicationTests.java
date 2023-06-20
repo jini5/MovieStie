b1e3615628849921a397c476205e929cf007fb16
jini5 <lim789789@gmail.com>
2023-06-21 01:40:59 +0900

    Initial commit

---
+++ src/test/java/com/example/moviestie/MovieStieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moviestie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieStieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
